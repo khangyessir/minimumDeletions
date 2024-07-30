f21ae2511a7c37bc749ecec4dd4a40926da7a2ce
khangyessir <tranphuckhang13102008@gmail.com>
2024-07-30 10:11:07 +0700

    first commit

---
+++ minimumDeletions.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int minimumDeletions(string s) {
+        int count = 0, ans = 0;
+        for(char c : s) {
+            if(c == 'b') count++;
+            else ans = min(ans + 1, count);
+        }
+        return ans;
+    }
+};
